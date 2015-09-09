639c209412d897b96512bb8b53c20264e8b012ab
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-09-09 21:10:00 +0200

    allows different Theorem: a cat has nine tails. Proof: 	No cat has eight tails. A cat has one tail more than no cat. 	Therefore, a cat has nine tails. paths

--- paiji2_comic/templatetags/fortune.py
+++ paiji2_comic/templatetags/fortune.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 import os
 
 
@@ -9,7 +10,11 @@ register = template.Library()
 )
 def get_fortune():
     try:
-        fortune = unicode(os.popen('fortune').read())
+        if settings.hasattr('FORTUNE_PATH'):
+            fortune_bin = settings.FORTUNE_PATH
+        else:
+            fortune_bin = '/usr/games/fortune'
+        fortune = unicode(os.popen(fortune_bin).read())
     except:
         fortune = u'Were they afraid that "e" being the most widely used letter in\nthe English language was going to war out thir xpnsiv kyboards if\nthy usd it all th tim?\n\n\t- Mike A. Harris on linux-kernel\n'
         print("fortune : error reading `fortune` command")
